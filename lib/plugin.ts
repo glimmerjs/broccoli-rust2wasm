cd9a1fed9d2d7a0fe385fd1dad195f097d590bd0
Kris Selden <kris.selden@gmail.com>
2017-11-29 20:14:07 -0800

    add Cargo.toml build support

--- lib/plugin.ts
+++ lib/plugin.ts
@@ -1,34 +1,57 @@
 import { execFileSync } from "child_process";
+import * as fs from "fs";
 import * as path from "path";
+
+const parseToml: (s: string) => any = require("toml").parse;
 
 export const Plugin: PluginStatic = require("broccoli-plugin");
 
 export interface RustPluginOptions {
-  entry: string;
+  entry?: string;
 }
 
 export default class RustPlugin extends Plugin {
-  private entry: string;
+  private entry: string | undefined;
 
-  constructor(input: any, options: RustPluginOptions) {
+  constructor(input: any, options?: RustPluginOptions) {
     super([input]);
-    this.entry = options.entry;
+    this.entry = options && options.entry;
   }
 
   public build() {
-    const inputFile = path.join(
-      this.inputPaths[0], this.entry);
 
-    const outputFile = path.join(
-      this.outputPath,
-      this.entry.replace(".rs", ".wasm"));
+    if (this.entry) {
+      const inputFile = path.join(
+        this.inputPaths[0], this.entry);
 
-    execFileSync(`rustc`, [
-      "--target", "wasm32-unknown-unknown",
-      "-O",
-      "--crate-type", "cdylib",
-      "-o", outputFile,
-      inputFile]);
+      const outputFile = path.join(
+        this.outputPath,
+        this.entry.replace(".rs", ".wasm"));
+
+      execFileSync(`rustc`, [
+        "--target", "wasm32-unknown-unknown",
+        "-O",
+        "--crate-type", "cdylib",
+        "-o", outputFile,
+        inputFile]);
+    } else {
+      const cargoToml = parseToml(
+        fs.readFileSync(path.join(this.inputPaths[0], "Cargo.toml"), "utf8"));
+      const packageName = cargoToml.package.name;
+      execFileSync("cargo", [
+        "build",
+        "--target", "wasm32-unknown-unknown",
+        "--release",
+      ], {
+        cwd: this.inputPaths[0],
+        env: Object.assign({}, process.env, {
+          CARGO_TARGET_DIR: this.cachePath,
+        }),
+      });
+      fs.writeFileSync(path.join(this.outputPath, packageName + ".wasm"),
+        fs.readFileSync(
+          path.join(this.cachePath, "wasm32-unknown-unknown", "release", packageName + ".wasm")));
+    }
   }
 }
 
--- test/index.ts
+++ test/index.ts
@@ -3,6 +3,62 @@ import * as fs from "fs";
 import Rust from "../index";
 
 QUnit.module("Rust", () => {
+  QUnit.test("works with a Cargo.toml file", async (assert) => {
+    const input = await createTempDir();
+    input.write({
+      "Cargo.toml": `
+[package]
+name = "hello_lib"
+version = "0.1.0"
+authors = ["Kris Selden <kris.selden@gmail.com>"]
+
+[lib]
+crate-type = ["cdylib"]
+
+[dependencies]
+`,
+      "src": {
+        "a.rs": `
+#[no_mangle]
+pub fn fibonacci(x: f64) -> f64 {
+  if x <= 2.0 {
+    return 1.0;
+  } else {
+    return fibonacci(x - 1.0) + fibonacci(x - 2.0);
+  }
+}`,
+      "lib.rs": `
+#![feature(lang_items)]
+#![no_std]
+
+#[lang = "panic_fmt"]
+#[no_mangle]
+pub extern fn panic_fmt() -> ! { loop {} }
+
+pub mod a;`,
+      },
+    });
+    try {
+      const plugin = new Rust(input.path());
+      const output = createBuilder(plugin);
+      try {
+        await output.build();
+
+        assert.deepEqual(output.changes(), {
+          "hello_lib.wasm": "create",
+        });
+        const buffer = fs.readFileSync(output.path("hello_lib.wasm"));
+        const mod = new WebAssembly.Module(buffer);
+        const instance = new WebAssembly.Instance(mod, {});
+        assert.strictEqual(instance.exports.fibonacci(20), 6765);
+      } finally {
+        output.dispose();
+      }
+    } finally {
+      input.dispose();
+    }
+  });
+
   QUnit.test("works with a entry file", async (assert) => {
     const input = await createTempDir();
     input.write({
